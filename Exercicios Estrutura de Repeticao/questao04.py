2b6db7c937edb284ee8e2dd7a8424a57572407f8
Lucas Ferreira <lucasnatacao@gmail.com>
2016-08-19 17:48:23 -0300

    Adicionando questao 4 e 8

---
+++ Exercicios Estrutura de Repeticao/questao04.py
@@ -0,0 +1,17 @@
+PaisA = 80000
+PaisB = 200000
+Anos = 0
+
+while (PaisA < PaisB):
+    PaisA = PaisA + 80000 * 0.03
+    PaisB = PaisB + 200000 * 0.015
+    Anos = Anos + 1
+
+else:
+    print("Será necessário",Anos,"anos necessários para que a população do país A ultrapasse ou iguale a população do país B")
+
+
+    
+
+    
+    
---
+++ Exercicios Estrutura de Repeticao/questao08.py
@@ -0,0 +1,25 @@
+Num1 = int(input("Digite um número:"))
+Num2 = int(input("Digite um número:"))
+Num3 = int(input("Digite um número:"))
+Num4 = int(input("Digite um número:"))
+Num5 = int(input("Digite um número:"))
+
+while (True):
+    Soma = Num1 + Num2 + Num3 + Num4 + Num5
+    Média = Soma/5
+    print("A soma é",Soma)
+    print("A média é",Média)
+    Num1 = int(input("Digite um número:"))
+    Num2 = int(input("Digite um número:"))
+    Num3 = int(input("Digite um número:"))
+    Num4 = int(input("Digite um número:"))
+    Num5 = int(input("Digite um número:"))
+    
+    
+    
+    
+    
+    
+
+    
+    
