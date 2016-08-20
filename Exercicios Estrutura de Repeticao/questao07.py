e4e76b3f0312ba61005eb673ceec3ac15739d0a4
Lucas Ferreira <lucasnatacao@gmail.com>
2016-08-20 13:46:22 -0300

    Adicionando questao 7

---
+++ Exercicios Estrutura de Repeticao/questao07.py
@@ -0,0 +1,14 @@
+Numero = int(input("Digite um número:"))
+Maior = Numero
+
+for N in range(0,4):
+    Numero = int(input("Digite um número:"))
+    if Maior < Numero:
+        Maior = Numero
+
+print(("O maior número é:"),Maior)
+        
+
+
+        
+
