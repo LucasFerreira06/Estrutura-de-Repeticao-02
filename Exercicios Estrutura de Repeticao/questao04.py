b75c69f3cc9ce1beb71000617a39e081591e6847
Lucas Ferreira <lucasnatacao@gmail.com>
2016-08-20 14:28:22 -0300

    Alterando questão 4

--- Exercicios Estrutura de Repeticao/questao04.py
+++ Exercicios Estrutura de Repeticao/questao04.py
@@ -1,17 +1,17 @@
-PaisA = 80000
-PaisB = 200000
+PaísA = 80000
+PaísB = 200000
 Anos = 0
 
-while (PaisA < PaisB):
-    PaisA = PaisA + 80000 * 0.03
-    PaisB = PaisB + 200000 * 0.015
+while (PaísA < PaísB):
+    print("Pop. do País A:", PaísA,"e Pop. do País B:", PaísB,"em", Anos,"anos")
+    PaísA = PaísA + PaísA * 0.03
+    PaísB = PaísB + PaísB * 0.015
     Anos = Anos + 1
-
-else:
-    print("Será necessário",Anos,"anos necessários para que a população do país A ultrapasse ou iguale a população do país B")
-
-
-    
-
-    
-    
+    continue
+    if (PaisA >= PaisB):
+        break
+            
+print("País A:",PaísA)
+print("Pais B:",PaísB)     
+print("Foram necessários",Anos,"anos para que a população do país A ultrapasse ou igualasse a população do país B")
+ 
